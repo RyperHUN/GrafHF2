eca4c58f1df3788405273c3118510d43c72b6b38
Ryper_Zsolt <akormanyzsolt@gmail.com>
2016-04-09 22:46:14 +0200

    mat4 class hozzáadva

--- Programs/Skeleton/Skeleton/VectorMath.h
+++ Programs/Skeleton/Skeleton/VectorMath.h
@@ -63,25 +63,79 @@ vec3 cross(const vec3& v1, const vec3& v2) {
 		(v1.z * v2.x) - (v1.x * v2.z),
 		v1.x * v2.y - v1.y * v2.x);
 }
-//FONTOS CSAK EGYSEG VEKTOROKKAL MUKODIK
-//Visszaverõdési irány határozza meg!
-//vec3 reflect(vec3 inDir, vec3 normal)
-//{
-//if (inDir.Length() > 1.1f || normal.Length() > 1.1f)
-//	throw "vec3::reflect() - Csak normalizalt vektorral mukodik a visszaverodes";
-//	return inDir - normal * dot(normal, inDir) *2.0f;
-//}
 
-//FONTOS CSAK EGYSEG VEKTOROKKAL MUKODIK
-//Törési irányt határozza meg!
-///TODO SmoothMaterial class-ba belerakni.
-//vec3 refract(vec3 inDir, vec3 normal) {
-//	if (inDir.Length() > 1.1f || normal.Length() > 1.1f)
-//		throw "vec3::refract() - Csak normalizalt vektorral mukodik a visszaverodes";
-//	float ior = n;
-//	float cosa = -dot(normal, inDir);
-//	if (cosa < 0) { cosa = -cosa; normal = -normal; ior = 1 / n; }
-//	float disc = 1 - (1 - cosa * cosa) / ior / ior;
-//	if (disc < 0) return reflect(inDir, normal);
-//	return inDir / ior + normal * (cosa / ior - sqrt(disc));
-//}
+// Utolsó oszlopba lesznek a dolgok tehát a Column Major formátumt használom + jobbrol kell vektorral szorozni
+class mat4 {
+public:
+	float m[4][4];
+	mat4(float m00 = 1, float m01 = 0, float m02 = 0, float m03 = 0,
+		float m10 = 0, float m11 = 1, float m12 = 0, float m13 = 0,
+		float m20 = 0, float m21 = 0, float m22 = 1, float m23 = 0,
+		float m30 = 0, float m31 = 0, float m32 = 0, float m33 = 1) {
+		m[0][0] = m00; m[0][1] = m01; m[0][2] = m02; m[0][3] = m03;
+		m[1][0] = m10; m[1][1] = m11; m[1][2] = m12; m[1][3] = m13;
+		m[2][0] = m20; m[2][1] = m21; m[2][2] = m22; m[2][3] = m23;
+		m[3][0] = m30; m[3][1] = m31; m[3][2] = m32; m[3][3] = m33;
+	}
+
+	mat4 operator*(const mat4& right) {
+		mat4 result;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				result.m[i][j] = 0;
+				for (int k = 0; k < 4; k++) result.m[i][j] += m[i][k] * right.m[k][j];
+			}
+		}
+		return result;
+	}
+	void eltolas(float x = 0, float y = 0, float z = 0)
+	{
+		//Utolso oszlop beallitasa
+		m[0][3] = x;
+		m[1][3] = y;
+		m[2][3] = z;
+	}
+	void projekcio(float x = 1, float y = 1, float z = 1)
+	{
+		m[0][0] = x;
+		m[1][1] = y;
+		m[2][2] = z;
+	}
+	void forgatZ(float szogFok)
+	{
+		//Valamiért kell egy minusz különben az óramutatóval ellentétesen forgat
+		float radian = -szogFok / 180 * M_PI;
+		m[0][0] = cosf(radian);
+		m[1][0] = sinf(radian);
+		m[0][1] = -sinf(radian);
+		m[1][1] = cosf(radian);
+	}
+	mat4 operator=(const mat4& right)
+	{
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				m[i][j] = right.m[i][j];
+			}
+		}
+		return (*this);
+	}
+	vec4 operator*(const vec4& right);
+	operator float*() { return &m[0][0]; }
+	vec3 operator*(const vec3& sima)
+	{
+		vec4 result;
+		vec4 right;
+		right.v[0] = sima.x;
+		right.v[1] = sima.y;
+		right.v[2] = sima.z;
+		for (int i = 0; i < 4; i++) {
+			result.v[i] = 0;
+			for (int j = 0; j < 4; j++) {
+
+				result.v[i] += m[i][j] * right.v[j];
+			}
+		}
+
+		return vec3(result.v[0], result.v[1], result.v[2]);
+	}
+};
